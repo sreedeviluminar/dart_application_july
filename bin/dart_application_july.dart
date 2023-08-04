8c4bfe3a826b1214c384ba386708143f87591d68
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-08-04 11:17:52 +0530

    first commit

---
+++ bin/dart_application_july.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_july/dart_application_july.dart' as dart_application_july;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_july.calculate()}!');
+}
